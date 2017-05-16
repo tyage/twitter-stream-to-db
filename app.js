dc8d096dfaea5efc15322724b3f61a368ecae12d
tyage <namatyage@gmail.com>
2017-05-16 11:07:52 +0900

    twitter stream fetch app

---
+++ app.js
@@ -0,0 +1,17 @@
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const stream = client.stream('statuses/filter', {track: 'javascript'});
+stream.on('data', function(event) {
+  console.log(event && event.text);
+});
+
+stream.on('error', function(error) {
+  throw error;
+});
