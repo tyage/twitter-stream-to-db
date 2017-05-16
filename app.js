6c764fc7962400f619bc23810ba25066c10971b0
tyage <namatyage@gmail.com>
2017-05-16 11:18:29 +0900

    save tweets to mysql

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
+const mysql = require('mysql');
 const Twitter = require('twitter');
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+connection.connect();
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,8 +20,14 @@ const client = new Twitter({
 const stream = client.stream('statuses/filter', {track: 'javascript'});
 stream.on('data', function(event) {
   console.log(event);
-});
 
+  const tweet = {
+    text: event.text
+  };
+  connection.query('INSERT INTO tweets SET ?', tweet, function (error, results, fields) {
+    if (error) throw error;
+  });
+});
 stream.on('error', function(error) {
   throw error;
 });
